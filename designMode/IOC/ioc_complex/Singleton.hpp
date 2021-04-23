f24051aad249b7c904088222feb5e15ff3ea561a
steve.he <steve.he@fjdynamics.com>
2021-04-23 13:31:43 +0800

    feat: 增加ioc的单例模式

--- designMode/IOC/ioc_complex/Singleton.hpp
+++ designMode/IOC/ioc_complex/Singleton.hpp
@@ -1,8 +1,18 @@
 #ifndef SINGLETON_H_
 #define SINGLETON_H_
 
+/*
+ * 给出一个在linux系统上可用的通过pthread_once来实现的线程安全的单例模式,通过pthread库来保证初始化只被执行一次
+ * 从而实现c++11 之前的单例也是线程安全的
+ * 在多线程编程环境下，尽管pthread_once()调用会出现在多个线程中，init_routine()函数仅执行一次，pthread_once是很适合用来实现线程安全单例。
+ */
+
 #include <iostream>
 #include <memory>
+#include <thread>
+#include <mutex>
+#include <cassert>
+#include <new>
 
 class noncopyable {
 protected:
@@ -15,44 +25,88 @@ protected:
 private:
     // 使用 delete 关键字禁止编译器自动产生复制构造函数和复制赋值操作符。
     noncopyable(const noncopyable&) = delete;
+    // 移动构造函数
+    noncopyable(noncopyable&&) = delete;
     const noncopyable& operator=(const noncopyable&) = delete;
 };
 
+//判断T 是否有Destory函数
 template<typename T>
-class Singleton:public noncopyable
-{
-public:
-    static T& getInstance() {
-        if (！value_) {
-            //在内存不足时，new(std::nothrow)并不抛出异常，而是将指针置NULL, std::nothrow可以实现对非零指针的检查;
-            value_ = new(std::nothrow) T();
-
-            if (value_) {
-                std::atexit(destory);   //程序结束时调用注册的函数
-            }
-        }
+struct isHasDestory {
+    template<typename U>
+    static constexpr bool Test(decltype(&U::Destory)*) {
+       return true; 
     }
 
-    static void destory() {
+    template<typename U>
+    static constexpr bool Test(...) {
+       return false; 
+    }
 
-        /*
-        * C++标准允许通过一个 delete 表达式删除指向不完全类的指针。如果该类有一个非平凡的析构函数，或者有一个类相关的 delete 操作符，
-        * 那么其行为就是无定义的，因此编译器作了这种扩展，以将这种未定义的行为转为编译期错误，帮助程序员们及早发现
-        * incomplete type呢，就是那些声明了，但没有定义的类型 class A; extern A a;
-        */
-        typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
-        T_must_be_complete_type dummy;
-        void(dummy);
+    static constexpr bool value = Test<T>(nullptr);
+};
+template <typename T>                                   
+constexpr bool isHasDestory<T>::value;
 
-        delete value_;
-        value_ = nullptr;
+template<typename T>
+typename std::enable_if<!isHasDestory<T>::value>::type CallDestory(T* instance) {
+    (void)instance;
+}
+
+template<typename T>
+typename std::enable_if<isHasDestory<T>::value>::type CallDestory(T* instance) {
+    instance->Destory();
+}
+
+template <typename T>
+class Singleton:private noncopyable
+{
+public:
+
+    static T& Instance() {
+        if (!instance) {
+            try{
+                pthread_once(&once_flag, [&]{
+                    //std::nothrow 在new空间不够的时候会返回空，并且不会扔出异常
+                    instance = new(std::nothrow) T();
+
+                    if (instance) {
+                        std::atexit(destorySelf);
+                    }
+                });
+            } catch(std::system_error e) {
+                std::cout << e.what() << std::endl;
+            }
+        }
+        return *instance;
     }
 
 private:
-    static T* value_;
+    
+    static void destorySelf() {
+        //call T's Destory
+        CallDestory(instance);
+
+
+        //如果T是不完整类型(只是声明没有定义的类)，char[-1]编译器将会报错，编译不通过
+        typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
+        T_must_be_complete_type dummy;
+        (void)dummy;
+
+        delete instance;
+        instance = nullptr;
+    }
+
+    static T* instance;
+    static pthread_once_t once_flag;
 };
 
-template<typename T>
-T* Singleton<T>::value_ = nullptr;
+template <typename T>
+T* Singleton<T>::instance = nullptr;
+
+template <typename T>
+pthread_once_t Singleton<T>::once_flag = PTHREAD_ONCE_INIT;
+
+
 
 #endif
