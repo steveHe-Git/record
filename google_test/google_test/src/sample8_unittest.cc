fe0e074ed76beb53fea45a36f458521629bbf287
199401027hjx <1137813212@qq.com>
2021-06-21 19:38:22 -0400

    feat: update googl test sample8

--- google_test/google_test/src/sample8_unittest.cc
+++ google_test/google_test/src/sample8_unittest.cc
@@ -37,7 +37,7 @@ public:
             next_prime = precalc_impl->GetNextPrime(p);
         }
 
-        return next_prime==-1?next_prime:OnTheFly_impl->GetNextPrime(p);
+        return next_prime!=-1?next_prime:OnTheFly_impl->GetNextPrime(p);
     }
 };
 
@@ -51,15 +51,54 @@ class PrimeTablesTest:public TestWithParam<::std::tuple<bool,int>>
 private:
     /* data */
 public:
-    PrimeTablesTest(/* args */);
-    ~PrimeTablesTest();
+
 protected:
-    
+    void SetUp() override {
+        bool force_on_the_fly;
+        int max_precalculated;
+        std::tie(force_on_the_fly, max_precalculated) = GetParam();
+        table = new HybridPrimeTable(force_on_the_fly, max_precalculated);
+    }
+
+    void TearDown() override {
+        delete table;
+        table = nullptr;
+    }
+
+    HybridPrimeTable* table;
 };
 
 
+TEST_P(PrimeTablesTest, ReturnsFalseForNonPrimes){
+    EXPECT_FALSE(this->table->IsPrime(-5));
+    EXPECT_FALSE(this->table->IsPrime(0));
+    EXPECT_FALSE(this->table->IsPrime(1));
+    EXPECT_FALSE(this->table->IsPrime(4));
+    EXPECT_FALSE(this->table->IsPrime(6));
+    EXPECT_FALSE(this->table->IsPrime(100));
+}
 
 
+TEST_P(PrimeTablesTest, ReturnTrueforPrime) {
+    EXPECT_TRUE(this->table->IsPrime(2));
+    EXPECT_TRUE(this->table->IsPrime(3));
+    EXPECT_TRUE(this->table->IsPrime(5));
+    EXPECT_TRUE(this->table->IsPrime(7));
+    EXPECT_TRUE(this->table->IsPrime(11));
+    EXPECT_TRUE(this->table->IsPrime(13));
+    EXPECT_TRUE(this->table->IsPrime(131));
+}
+
+TEST_P(PrimeTablesTest, CanGetNextPrime) {
+    EXPECT_EQ(2, this->table->GetNextPrime(0));
+    EXPECT_EQ(3, this->table->GetNextPrime(2));
+    EXPECT_EQ(5, this->table->GetNextPrime(3));
+    EXPECT_EQ(7, this->table->GetNextPrime(5));
+    EXPECT_EQ(131, this->table->GetNextPrime(128));
+}
+
+INSTANTIATE_TEST_SUITE_P(MeaningfulTestParameters, PrimeTablesTest, 
+    Combine(Bool(), Values(1, 10)));
 
 
 }//namespace
